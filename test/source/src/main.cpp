e44c7e4a805ad450513786092491c2ad696eaa8d
leosocy <513887568@qq.com>
2017-12-28 23:54:58 +0800

    完善目录结构

---
+++ test/source/src/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+> File Name: main.cpp
+> Author: Leosocy
+> Mail: 513887568@qq.com
+> Created Time: 2017/12/28 23:51:38
+************************************************************************/
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    testing::GTEST_FLAG(filter) = "*";
+    return RUN_ALL_TESTS();
+}
